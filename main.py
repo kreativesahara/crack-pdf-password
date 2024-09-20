167a3a3472d0700bccef49ff139003c0bf1eb8a5
kreativesahara <158110796+kreativesahara@users.noreply.github.com>
2024-09-20 17:11:23 +0300

    Update main.py
    Added: a functionality on main.py to save the the decrypted pdf version as unlocked.pdf

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pikepdf
 import time
 from colorama import Fore
 
-password_file = open("yourPasswordFileName.txt")
+password_file = open("passwordfile.txt")
 
 i = 0
 start_time = time.time()
@@ -10,8 +10,11 @@ for password in password_file:
     i += 1
     print(Fore.RED+f"\r {i} Password is tested {password.strip()}", end = "")
     try:
-        pikepdf.open("YourPDFFileNmae.pdf",password = password.strip("\n"))
-        end_time = time.time()
+        with pikepdf.open("2024.pdf",password = password.strip("\n")) as pdf:
+            # Added this line to save the decrypted vision of the file inside our project folder.
+            extract = pdf.save("unlocked.pdf")
+            end_time = time.time()
+
         print("\n")
         print(Fore.GREEN+ f"password found in {str(end_time-start_time)[:4]} second \nPassword is: ", end = "")
         print(Fore.BLUE+ f" {password}")
